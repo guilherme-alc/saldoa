fafbd11911e5fb630b7ca18ece5a31808413ef0a
guilherme-alc <guilhermealc01@gmail.com>
2025-12-19 23:11:31 -0300

    Cria e implementa interface para transações

---
+++ MeuBolso.Application/Transactions/Abstractions/ITransactionRepository.cs
@@ -0,0 +1,14 @@
+using MeuBolso.Application.Common.Pagination;
+using MeuBolso.Domain.Entities;
+
+namespace MeuBolso.Application.Transactions.Abstractions;
+
+public interface ITransactionRepository
+{
+    Task AddAsync(Transaction transaction);
+    Task UpdateAsync(Transaction transaction);
+    Task RemoveAsync(Transaction transaction);
+    Task<Transaction?> GetByIdAsync(long id, string userId);
+    Task<Transaction?> GetByIdForUpdateAsync(long id, string userId);
+    Task<PagedResult<Transaction>> ListByPeriodAsync(string userId, DateTime startDate, DateTime endDate, int pageNumber, int pageSize);
+}
---
+++ MeuBolso.Infrastructure/Transactions/TransactionRepository.cs
@@ -0,0 +1,63 @@
+using MeuBolso.Application.Common.Pagination;
+using MeuBolso.Application.Transactions.Abstractions;
+using MeuBolso.Domain.Entities;
+using MeuBolso.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuBolso.Infrastructure.Transactions;
+
+public class TransactionRepository : ITransactionRepository
+{
+    private readonly MeuBolsoDbContext _dbContext;
+    public TransactionRepository(MeuBolsoDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task AddAsync(Transaction transaction)
+    {
+        await _dbContext.Transactions.AddAsync(transaction);
+    }
+
+    public async Task UpdateAsync(Transaction transaction)
+    {
+        _dbContext.Transactions.Update(transaction);
+    }
+
+    public async Task RemoveAsync(Transaction transaction)
+    {
+        _dbContext.Transactions.Remove(transaction);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<Transaction?> GetByIdAsync(long id, string userId)
+    {
+        return await _dbContext.Transactions
+            .AsNoTracking()
+            .FirstOrDefaultAsync(t => t.Id == id && t.UserId == userId);
+    }
+
+    public async Task<Transaction?> GetByIdForUpdateAsync(long id, string userId)
+    {
+        return await _dbContext.Transactions
+            .FirstOrDefaultAsync(t => t.Id == id && t.UserId == userId);
+    }
+
+    public async Task<PagedResult<Transaction>> ListByPeriodAsync(string userId, DateTime startDate, DateTime endDate, int pageNumber, int pageSize)
+    {
+        var query = _dbContext
+            .Transactions
+            .AsNoTracking()
+            .Where(t => t.UserId == userId);
+        
+        var total = await query.CountAsync();
+
+        var data = await query
+            .OrderByDescending(t => t.PaymentOrReceivedAt)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+        
+        return new PagedResult<Transaction>(data, total, pageNumber, pageSize);
+    }
+}
