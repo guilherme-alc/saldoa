15764ec4020d920f43dbffd811ef91cd129de42b
guilherme-alc <guilhermealc01@gmail.com>
2026-01-06 07:08:13 -0300

    Adiciona helper para gerar, hashear e converter o refresh token

---
+++ MeuBolso.Application/Auth/RefreshTokenCrypto.cs
@@ -0,0 +1,25 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MeuBolso.Application.Auth;
+
+public static class RefreshTokenCrypto
+{
+    public static string GenerateToken() //client
+    {
+        var bytes = RandomNumberGenerator.GetBytes(64);
+        return Base64UrlEncode(bytes);
+    }
+    
+    public static string HashToken(string token) //banco
+    {
+        var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(token));
+        return Convert.ToHexString(bytes);
+    }
+    
+    private static string Base64UrlEncode(byte[] bytes)
+        => Convert.ToBase64String(bytes)
+            .Replace("+", "-")
+            .Replace("/", "_")
+            .TrimEnd('=');
+}
