ce06d39a8c5d6baf75eced2b05f40aab368a1b98
guilherme-alc <guilhermealc01@gmail.com>
2026-01-06 07:06:52 -0300

    Mudança de retorno do do registro

--- MeuBolso.Application/Auth/Register/RegisterUseCase.cs
+++ MeuBolso.Application/Auth/Register/RegisterUseCase.cs
@@ -1,5 +1,4 @@
 using MeuBolso.Application.Auth.Abstractions;
-using MeuBolso.Application.Auth.AuthDTO;
 using MeuBolso.Application.Common.Results;
 using MeuBolso.Application.Identity.Abstractions;
 
@@ -9,28 +8,22 @@ public class RegisterUseCase
 {
     private readonly IIdentityService _identityService;
     private readonly IJwtProvider _jwtProvider;
+
     public RegisterUseCase(IIdentityService identityService, IJwtProvider jwtProvider)
     {
         _identityService = identityService;
         _jwtProvider = jwtProvider;
     }
-    public async Task<Result<AuthResponse>> ExecuteAsync(RegisterRequest request)
+    public async Task<Result<string>> ExecuteAsync(RegisterRequest request)
     {
         if (await _identityService.UserExistsAsync(request.Email))
-            return Result<AuthResponse>.Failure("Usuário já existe");
+            return Result<string>.Failure("Usuário já existe");
         
         var userId = await _identityService.CreateUserAsync(
             request.Email, 
             request.Password, 
             request.FullName);
-        
-        var accessToken = _jwtProvider.GenerateToken(
-            userId: userId,
-            email: request.Email,
-            claims: []);
 
-        return Result<AuthResponse>.Success(
-            new AuthResponse(accessToken, "", DateTime.UtcNow.AddMinutes(15))
-        );
+        return Result<string>.Success(userId);
     }
 }
