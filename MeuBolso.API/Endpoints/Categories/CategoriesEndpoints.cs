93b1b7e0b59ea54786368a53806ca871f4f7761b
guilherme-alc <guilhermealc01@gmail.com>
2026-01-18 10:36:11 -0300

    Adiciona mapeamento do endpoint de delete category

--- MeuBolso.API/Endpoints/Categories/CategoriesEndpoints.cs
+++ MeuBolso.API/Endpoints/Categories/CategoriesEndpoints.cs
@@ -11,5 +11,6 @@ public static class CategoriesEndpoints
         CreateCategoryEndpoint.Map(group);
         UpdateCategoryEndpoint.Map(group);
         GetCategoryByIdEndpoint.Map(group);
+        DeleteCategoryEndpoint.Map(group);
     }
 }
