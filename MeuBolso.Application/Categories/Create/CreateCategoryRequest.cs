7cfe2b9479857b55e71943eaa01aafe357b80e13
guilherme-alc <guilhermealc01@gmail.com>
2026-01-19 21:30:21 -0300

    Ajuste nos create requests

--- MeuBolso.Application/Categories/Create/CreateCategoryRequest.cs
+++ MeuBolso.Application/Categories/Create/CreateCategoryRequest.cs
@@ -1,5 +1,3 @@
-using MeuBolso.Application.Common.Security;
-
 namespace MeuBolso.Application.Categories.Create;
 
-public sealed record CreateCategoryRequest ( string Name, string? Description, string? Color);
+public sealed record CreateCategoryRequest (string Name, string? Description, string? Color);
--- MeuBolso.Application/Transactions/Create/CreateTransactionRequest.cs
+++ MeuBolso.Application/Transactions/Create/CreateTransactionRequest.cs
@@ -1,14 +1,5 @@
-using MeuBolso.Application.Common.Security;
 using MeuBolso.Domain.Enums;
 
 namespace MeuBolso.Application.Transactions.Create;
 
-public class CreateTransactionRequest : AuthenticatedRequest
-{
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public DateTime? PaidOrReceivedAt { get; set; }
-    public ETransactionType Type { get; set; }
-    public decimal Amount { get; set; }
-    public long CategoryId { get; set; }
-}
+public sealed record CreateTransactionRequest(string Title, string? Description, DateOnly? PaidOrReceivedAt, ETransactionType Type, decimal Amount, long CategoryId);
