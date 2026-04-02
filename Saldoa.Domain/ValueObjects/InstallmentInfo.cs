7e0ba414bde543a1cbb207e793176aaad043d011
guilherme-alc <guilhermealc01@gmail.com>
2026-04-01 22:21:48 -0300

    feat: cria value object InstallmentInfo

---
+++ Saldoa.Domain/ValueObjects/InstallmentInfo.cs
@@ -0,0 +1,40 @@
+﻿namespace Saldoa.Domain.ValueObjects
+{
+    public class InstallmentInfo
+    {
+        public int TotalInstallments { get; }
+        public int InstallmentNumber { get; }
+        public Guid? InstallmentGroupId { get; } = null;
+
+        private InstallmentInfo(int totalInstallments, int installmentNumber, Guid? installmentGroupId) 
+        {
+            TotalInstallments = totalInstallments;
+            InstallmentNumber = installmentNumber;
+            InstallmentGroupId = installmentGroupId;
+        }
+
+        public static InstallmentInfo Single()
+        {
+            return new InstallmentInfo(
+                totalInstallments: 1,
+                installmentNumber: 1,
+                installmentGroupId: null
+            );
+        }
+
+        public static InstallmentInfo Create(int totalInstallments, int installmentNumber, Guid? installmentGroupId)
+        {
+            if (totalInstallments <= 1)
+                throw new ArgumentOutOfRangeException(nameof(totalInstallments), "Parcelamento deve ser maior que 1.");
+
+            if (installmentNumber <= 0 || installmentNumber > totalInstallments)
+                throw new ArgumentOutOfRangeException(nameof(installmentNumber), "Número da parcela inválido.");
+
+            return new InstallmentInfo(
+                totalInstallments,
+                installmentNumber,
+                installmentGroupId
+            );
+        }
+    }
+}
